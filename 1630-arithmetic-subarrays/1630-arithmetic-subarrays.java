71c27f27623b942513bafb83a5b7f3ded2a24e9f
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-04-20 19:05:55 +0300

    Time: 104 ms (6.36%), Space: 54.9 MB (33.23%) - LeetHub

---
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.java
@@ -0,0 +1,33 @@
+class Solution {
+    
+    public static boolean checkArithmetic(Integer[] arr)
+	{
+		int intrvl = arr[1] - arr[0];
+		for (int i = 0; i < arr.length - 1; i++)
+		{
+			if (arr[i+1] - arr[i] != intrvl)
+				return false;
+		}
+		return true;
+	}
+    
+    public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
+        List<Boolean> ans = new ArrayList<Boolean>();
+		
+		for (int i = 0; i < l.length; i++)
+		{
+			int siz = r[i] - l[i] + 1;
+			Integer arr[] = new Integer[siz];
+			int indx = 0;
+			for (int j = l[i]; j <= r[i]; j++)
+			{
+				arr[indx] = nums[j];
+				indx++;
+			}
+			Arrays.sort(arr, Collections.reverseOrder());
+			ans.add(checkArithmetic(arr));
+		}
+		
+		return ans;
+    }
+}
