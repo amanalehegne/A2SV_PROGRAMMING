dfdd9a3f939898388d5e8a5147d3258035b2cded
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-07-01 17:45:05 +0300

    Time: 20 ms (44.94%), Space: 50.5 MB (54.05%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -3,26 +3,24 @@ class Solution {
 		
 		HashMap<Integer, Integer> map = new HashMap();
 		
-		Arrays.sort(nums);
-		
-		System.out.println(Arrays.toString(nums));
-		
 		for(int num : nums)
 			map.put(num, map.getOrDefault(num, 0) + 1);
 		
-		int maxKey = 0;
-		int ans[] = new int[k];
-		for(int i = 0; i < k; i++) {
-			int max = 0;
-			for(int key : map.keySet()) {
-				if (max <= map.get(key)) {
-					max = map.get(key);
-					maxKey = key;
-				}
-			}
-			map.remove(maxKey);
-			ans[i] = maxKey;
+		List<int[]> list = new ArrayList();
+		
+		for(int key : map.keySet()) {
+			list.add(new int[] {key, map.get(key)});
 		}
+		
+		Collections.sort(list,(a,b)->b[1]-a[1]);
+
+		
+		int ans[] = new int[k];
+		
+		for (int i = 0; i < k; i++) {
+			ans[i] = list.get(i)[0];
+		}
+		
 		
 		return ans;
 	}
