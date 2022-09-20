6f0f0bae9b77e43185035e5050f6cf62de78ef5a
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-09-21 00:20:36 +0300

    Time: 74 ms (31.18%), Space: 13.9 MB (55.35%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,6 +1,14 @@
 class Solution:
-    def fib(self, n: int) -> int:
+    def ans(self, n, map):
         if n == 0 or n == 1:
             return n
-        return self.fib(n-1) + self.fib(n-2)
+        if map.get(n):
+            return map[n]
+        else:
+            map[n] = self.ans(n - 1, map) + self.ans(n - 2, map)
+        return map[n]
+    
+    def fib(self, n: int) -> int:
+        map = {}
+        return self.ans(n, map)
         
