833d5527878b6b9d4c4611a5d262f6a1da1d775c
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-09-28 10:47:15 +0300

    Time: 907 ms (5.00%), Space: 16.7 MB (33.99%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
@@ -3,12 +3,10 @@ class Solution:
         count, current_sum, dic = 0, 0, {}
         for i in range(len(nums)):
             current_sum += nums[i]
-            val = current_sum - k
             if current_sum == k:
                 count += 1
-            if dic.get(val):
-                count += dic.get(val)
-
+            if dic.get(current_sum - k):
+                count += dic.get(current_sum - k)
             if dic.get(current_sum):
                 dic[current_sum] += 1
             else:
