3e6374f8fc282929252d9b1950ec1564a3c01658
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-10-09 15:49:28 +0300

    Time: 441 ms (12.85%), Space: 15.2 MB (48.42%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.py
+++ 724-find-pivot-index/724-find-pivot-index.py
@@ -1,12 +1,12 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        prefix, ans = [], 0
+        prefix = [0]
+        # Create the prefix sum array
         for i in nums:
-            ans += i
-            prefix.append(ans)
-        for i, val in enumerate(nums):
-            prev = prefix[i] - val
-            next_ = prefix[len(prefix) - 1] - prefix[i]
-            if prev == next_:
+            prefix.append(prefix[-1] + i)
+        i = 0
+        while i < len(prefix) - 1:
+            if prefix[i] == prefix[-1] - prefix[i + 1]:
                 return i
+            i += 1
         return -1
