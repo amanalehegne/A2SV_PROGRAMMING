58a47ed26140a35bf293c5afce84f17de65a2ea3
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-10-09 11:36:01 +0300

    Time: 1249 ms (12.50%), Space: 20.9 MB (74.35%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.py
+++ 881-boats-to-save-people/881-boats-to-save-people.py
@@ -1,16 +1,14 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
-        boat, l, r = 0, 0, len(people) - 1
         people.sort()
+        l, r = 0, len(people) - 1
+        count = 0
         while l <= r:
-            # The Smallest Weight person has the most option and the Highest Weight person has the least
-            # If their weight is less or equal to limit, they will go together.
-            # If their weight is more than the max, the Maximum weight person has l=no option left to go with so goes alone
-            if people[l] + people[r] <= limit:
+            if people[r] + people[l] <= limit:
+                count += 1
                 l += 1
                 r -= 1
-            # Now the Maximum weight person has changed, so we checked and repeat the process
             else:
+                count += 1
                 r -= 1
-            boat += 1
-        return boat
+        return count
