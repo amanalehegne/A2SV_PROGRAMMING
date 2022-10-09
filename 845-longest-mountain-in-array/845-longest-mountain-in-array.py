b108769a8abf989cdf2d7f0bc3b9da3e61d50db9
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-10-09 12:44:57 +0300

    Time: 319 ms (53.44%), Space: 15.2 MB (81.06%) - LeetHub

--- 845-longest-mountain-in-array/845-longest-mountain-in-array.py
+++ 845-longest-mountain-in-array/845-longest-mountain-in-array.py
@@ -7,12 +7,15 @@ class Solution:
             while i < len(arr) - 1 and arr[i] < arr[i + 1]:
                 i += 1
                 rise = True
+            # There must be rising before falling, else that wont be a mountain, it would be a valley
             if rise:
                 while i < len(arr) - 1 and arr[i] > arr[i + 1]:
                     i += 1
                     fall = True
             if rise and fall:
                 mountain_size = max(mountain_size, i - l + 1)
+            # If we have no mountain starting at index i, we check the next index. In other cases however, we began from the end of the mountain. 
+            # In this case however we have no mountain and check the next index point (i += 1)
             else:
                 i += 1
             rise = fall = False
