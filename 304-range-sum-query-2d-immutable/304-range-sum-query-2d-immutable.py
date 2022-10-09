8d58da97479aaa6c784a441f3f02f931dcc95c16
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-10-09 17:43:45 +0300

    Time: 3315 ms (38.92%), Space: 24.9 MB (38.82%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.py
@@ -1,24 +1,23 @@
 class NumMatrix:
 
     def __init__(self, matrix):
-        rows, cols = len(matrix), len(matrix[0])
-        self.prefix_sum = [[0] * (cols + 1) for r in range(rows + 1)]
+        row, col = len(matrix), len(matrix[0])
+        self.prefix_sum = [[0] * (col + 1) for i in range(row + 1)]
 
-        # Prefix sum
-        for r in range(rows):
-            prefix = 0
-            for c in range(cols):
-                prefix += matrix[r][c]
-                above = self.prefix_sum[r][c + 1]
-                self.prefix_sum[r + 1][c + 1] = prefix + above
+        # Create box prefix sum
+        for r in range(row):
+            prefix_sum = 0
+            for c in range(col):
+                prefix_sum += matrix[r][c]
+                self.prefix_sum[r + 1][c + 1] = prefix_sum + self.prefix_sum[r][c + 1]
 
-    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        total_area = self.prefix_sum[row2 + 1][col2 + 1]
-        num1 = self.prefix_sum[row1][col2 + 1]
-        num2 = self.prefix_sum[row2 + 1][col1]
-        left_out = self.prefix_sum[row1][col1]
-        return total_area + left_out - num1 - num2
-
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int):
+        area_sum = self.prefix_sum[row2 + 1][col2 + 1]
+        remove_top = self.prefix_sum[row1][col2 + 1]
+        remove_side = self.prefix_sum[row2 + 1][col1]
+        double_removed = self.prefix_sum[row1][col1]
+        return area_sum - remove_top - remove_side + double_removed
+        
 
 
 # Your NumMatrix object will be instantiated and called as such:
