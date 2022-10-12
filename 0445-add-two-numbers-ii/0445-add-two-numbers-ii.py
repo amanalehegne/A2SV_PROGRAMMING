f3dc754abbe5511a50431cfd2048b97faccace9f
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-10-12 13:35:43 +0300

    Time: 158 ms (26.10%), Space: 13.9 MB (46.15%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
@@ -12,8 +12,8 @@ class Solution:
         while l2:
             num2 += str(l2.val)
             l2 = l2.next
-        sum = int(num1) + int(num2)
-        ans = [ListNode(i) for i in str(sum)]
+        add = str(int(num1) + int(num2))
+        ans = [ListNode(i) for i in add]
         for i in range(len(ans) - 1):
             ans[i].next = ans[i + 1]
         return ans[0]
