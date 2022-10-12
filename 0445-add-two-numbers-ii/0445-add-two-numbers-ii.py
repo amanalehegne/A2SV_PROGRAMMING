3f9107588fe64d65529846e11afdc0535f89877d
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-10-12 13:52:10 +0300

    Time: 172 ms (11.81%), Space: 14 MB (46.15%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
@@ -5,15 +5,38 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        num1 = num2 = ""
+        s1, s2 = [], []
         while l1:
-            num1 += str(l1.val)
+            s1.append(l1.val)
             l1 = l1.next
         while l2:
-            num2 += str(l2.val)
-            l2 = l2.next
-        add = str(int(num1) + int(num2))
-        ans = [ListNode(i) for i in add]
-        for i in range(len(ans) - 1):
-            ans[i].next = ans[i + 1]
-        return ans[0]
+           s2.append(l2.val)
+           l2 = l2.next
+        print(s1, s2)
+        # Sum - Adding the stacks
+        add, carry = [], 0
+        while s1 and s2:
+            get_sum = s1.pop() + s2.pop() + carry
+            carry = get_sum // 10
+            get_sum = get_sum % 10
+            add.append(get_sum)
+        while s1:
+            get_sum = s1.pop() + carry
+            carry = get_sum // 10
+            get_sum = get_sum % 10
+            add.append(get_sum)
+        while s2:
+            get_sum = s2.pop() + carry
+            carry = get_sum // 10
+            get_sum = get_sum % 10
+            add.append(get_sum)
+        if carry:
+            add.append(carry)
+
+        list = []
+        for i in range(1, len(add) + 1):
+            list.append(ListNode(add[-i]))
+        for i in range(len(list) - 1):
+            list[i].next = list[i + 1]
+
+        return list[0]
