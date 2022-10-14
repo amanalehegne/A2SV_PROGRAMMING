0b42bc4175dc9d5ac6b2c0cc2c48def93a846e91
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-10-14 15:31:29 +0300

    Time: 3561 ms (24.89%), Space: 30.7 MB (11.24%) - LeetHub

--- 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.py
+++ 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.py
@@ -3,6 +3,9 @@ class Solution:
         nums.sort()
         ans = []
         l, r = 0, len(nums) - 1
+        # To guarentee the number is larger than the avergae of its neibour, the number have to be greater than both its neightbers. 
+        # To do that we put the largest element (nums[-1]) between the the two smallest (nums[0] and nums[1]). Then repeat the process
+        # To get the largest number and the smallest number easily we sorted the list
         while l <= r:
             ans.append(nums[l])
             if l != r:
