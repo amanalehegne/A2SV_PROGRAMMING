39629f9e9a4365f28951ef8d146b6858fc342817
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-10-15 10:26:17 +0300

    Time: 47 ms (87.93%), Space: 13.8 MB (97.27%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        def compare(n1, n2):
+            if str(n1) + str(n2) > str(n2) + str(n1):
+                return -1
+            else:
+                return 1
+        nums.sort(key=cmp_to_key(compare))
+        ans = ""
+        for i in nums:
+            ans += str(i)
+        return str(int(ans))
