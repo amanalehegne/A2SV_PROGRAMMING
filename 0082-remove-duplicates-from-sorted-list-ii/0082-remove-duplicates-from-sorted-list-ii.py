9df8754d6f8f14cba3103572c6791a2ebce5b286
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-10-16 17:47:35 +0300

    Time: 87 ms (27.46%), Space: 13.8 MB (98.31%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
@@ -6,6 +6,10 @@
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         dummy = ListNode(0, head)
+        # The 'prev' is used as an anchor - to hook the element to the next non repeating element
+        # 'cur' along with 'cur.next' is used to find non repeating elements
+        # This was we have 3 pointers - prev, cur, cur.next. if cur and cur.next are similar, they move until they become unique
+        # once we find a unique element we attach it to our previous(prev) element
         prev, cur = dummy, head
         while cur and cur.next:
             if cur.val == cur.next.val:
