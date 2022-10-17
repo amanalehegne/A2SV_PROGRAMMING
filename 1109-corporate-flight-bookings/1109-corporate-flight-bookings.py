6c85be79d0ddd762d2299ee1b19781eab02a04c1
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-10-17 12:39:35 +0300

    Time: 1254 ms (71.15%), Space: 28.4 MB (23.10%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.py
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.py
@@ -1,16 +1,11 @@
 class Solution:
     def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
-        ans = [0] * n
-        for i, j, k in bookings:
-            ans[i - 1] += k
-            # Limit the seat to the start of the booking to the end, 
-            # by giving the flight next to the end a -ve value of the seat
-            # so that when we use predix sum, the prefix sum value of the seat dies (not get accounted) 
-            if j < n:
-                ans[j] -= k
-        # Calculate the prefix Sum
-        prefix_sum = 0
+        ans = [0] * (n + 1)
+        for start, end, val in bookings:
+            ans[start - 1] += val
+            ans[end] += -val
+        prefix = 0
         for i, val in enumerate(ans):
-            prefix_sum += val
-            ans[i] = prefix_sum
-        return ans
+            prefix += val
+            ans[i] = prefix
+        return ans[:-1]
