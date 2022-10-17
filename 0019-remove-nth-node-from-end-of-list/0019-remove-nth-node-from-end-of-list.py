02f1cd578f77a5b7f74ad7f5683a05137989eb6e
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-10-17 15:18:23 +0300

    Time: 62 ms (48.75%), Space: 13.9 MB (71.05%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -10,17 +10,12 @@ class Solution:
         while temp:
             count += 1
             temp = temp.next
-        
-        if count == 0:
-            return None
+
         if count == n:
             return head.next
         
         temp = head
         for i in range(count - n - 1):
             temp = temp.next
-        if temp.next and temp.next.next:
-            temp.next = temp.next.next
-        else:
-            temp.next = None
+        temp.next = temp.next.next
         return head
