81a6113a3eae17b36767ea45b247f019c05b58c0
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-10-17 12:59:07 +0300

    Time: 145 ms (58.41%), Space: 14.5 MB (11.46%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,20 +1,14 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        # Get the maximum distance
-        n = trips[-1][-1]
-        for i in trips:
-            n = max(n, i[-1])
-        ans = [0] * (n + 1)
-        # Get number of people in the car at the start of their trip and mark their ending (using negative)
-        for number, start, end in trips:
-            ans[start] += number
-            ans[end] -= number
-        # Prefix Sum
-        # Get the number of people in the car at that point, if it exceeds the capacity, return False
-        prefix_sum = 0
-        for i in range(len(ans)):
-            prefix_sum += ans[i]
-            if prefix_sum > capacity:
+        car_end = prefix = 0
+        for num, start, end in trips:
+            car_end = max(car_end, end)
+        ans = [0] * (car_end + 1)
+        for num, start, end in trips:
+            ans[start] += num
+            ans[end] += -num
+        for i, val in enumerate(ans):
+            prefix += val
+            if prefix > capacity:
                 return False
-            ans[i] = prefix_sum
         return True
