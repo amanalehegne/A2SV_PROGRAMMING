171f94608b5abaf0f1662760e302b849ae3952ad
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-10-17 13:13:44 +0300

    Time: 1453 ms (56.31%), Space: 28 MB (71.66%) - LeetHub

--- 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.py
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.py
@@ -1,20 +1,10 @@
 class Solution:
     def chalkReplacer(self, chalk: List[int], k: int) -> int:
-        total_sum = 0
-        # Get the total number of calks needed, for a full cycle
+        sumVal = 0
+        for i in chalk:
+            sumVal += i
+        k = k % sumVal
         for i, val in enumerate(chalk):
-            total_sum += val
-            # However sometimes the total number of calks isn't enough to cover a whole round
-            if total_sum > k:
+            if k < val:
                 return i
-
-        # The number of calks remaining after certain round is found
-        # by getting the remainder of total chalk remaining to the number of calks needed for a cycle
-        total_sum = k % total_sum
-
-        prefix_sum = 0
-        for i, val in enumerate(chalk):
-            prefix_sum += val
-            # Check how many chalks we have left for the partial cycle, and get the index when it's not enough
-            if prefix_sum > total_sum:
-                return i
+            k -= val
