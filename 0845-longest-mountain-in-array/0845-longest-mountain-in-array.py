e2f843e28a2a5d41411b4512d83b413d169d2f02
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-10-18 12:32:36 +0300

    Time: 472 ms (18.08%), Space: 15.3 MB (45.35%) - LeetHub

--- 0845-longest-mountain-in-array/0845-longest-mountain-in-array.py
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.py
@@ -1,19 +1,20 @@
 class Solution:
     def longestMountain(self, arr: List[int]) -> int:
         rise = fall = False
-        i = mountain = 0
-        while i < len(arr) - 1:
-            temp = i
-            while i + 1 < len(arr) and arr[i] < arr[i+1]:
+        i = count = 0
+        while i < len(arr):
+            start = i
+            while i + 1 < len(arr) and arr[i] < arr[i + 1]:
                 i += 1
                 rise = True
             if rise:
-                while i + 1 < len(arr) and arr[i] > arr[i+1]:
+                while i + 1 < len(arr) and arr[i] > arr[i + 1]:
                     i += 1
                     fall = True
             if rise and fall:
-                mountain = max(mountain, i - temp + 1)
+                count = max(count, i - start + 1)
             else:
                 i += 1
             rise = fall = False
-        return mountain
+        return count
+        
