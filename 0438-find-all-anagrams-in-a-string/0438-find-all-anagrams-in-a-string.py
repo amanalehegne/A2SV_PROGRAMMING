6d39e670d7925a12a4afb9aaafde0b3785b135b3
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-10-18 14:48:25 +0300

    Time: 431 ms (31.57%), Space: 15.3 MB (5.26%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,42 +1,26 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        if len(p) > len(s):
-            return []
-        dic = dict()
-        temp = dict()
-        for i in p:
-            if dic.get(i) is not None:
-                dic[i] += 1
-            else:
-                dic[i] = 1
-
-        for r in range(len(p)):
-            if temp.get(s[r]) is not None:
-                temp[s[r]] += 1
-            else:
-                temp[s[r]] = 1
-        l, r = 0, r + 1
-        ans = []
-        while r < len(s):
+        if len(s) >= len(p):
+            dic, temp = dict(), dict()
+            for i in p:
+                dic[i] = 1 + dic.get(i, 0)
+            for r in range(len(p)):
+                temp[s[r]] = 1 + temp.get(s[r], 0)
+            l, r, ans = 0, r + 1, []
+            while r < len(s):
+                if self.compareMaps(dic, temp):
+                    ans.append(l)
+                temp[s[l]] -= 1
+                if temp[s[l]] == 0:
+                    temp.pop(s[l])
+                temp[s[r]] = 1 + temp.get(s[r], 0)
+                l, r = l+1, r+1
             if self.compareMaps(dic, temp):
                 ans.append(l)
-            temp[s[l]] -= 1
-            if temp[s[l]] == 0:
-                temp.pop(s[l])
-            if temp.get(s[r]) is not None:
-                temp[s[r]] += 1
-            else:
-                temp[s[r]] = 1
-            l += 1
-            r += 1
-        if self.compareMaps(dic, temp):
-            ans.append(l)
-        return ans
+            return ans
     
     def compareMaps(self, dic1, dic2):
         for key in dic1:
-            if dic2.get(key) is None:
-                return False
-            if dic1[key] != dic2[key]:
+            if (dic2.get(key) is None) or (dic1[key] != dic2[key]):
                 return False
         return True
