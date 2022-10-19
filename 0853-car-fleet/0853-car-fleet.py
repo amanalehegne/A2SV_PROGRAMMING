37b5d9612d3b374e1609765bd0bfeb82606928b5
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-10-19 14:06:39 +0300

    Time: 1343 ms (74.76%), Space: 38.4 MB (9.08%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,18 +1,11 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        dic = {}
+        dic = dict()
         for i in range(len(position)):
-            dic[position[i]] = speed[i]
+            dic[position[i]] = (target - position[i]) / speed[i]
         position.sort()
-
-        fleets = 0
-        prev_car = -1
+        stack = []
         for i in range(1, len(position) + 1):
-            # arrival time is remaining distance divided by speed you're going
-            # arrival time = (destination - current position) / speed
-            # If two cars arrive at the same time, they are considered as a single fleet
-            arrival_time = (target - position[-i]) / dic.get(position[-i])
-            if arrival_time > prev_car:
-                fleets += 1
-                prev_car = arrival_time
-        return fleets
+            if (not stack) or (stack[-1] < dic[position[-i]]):
+                stack.append(dic[position[-i]])
+        return len(stack)
