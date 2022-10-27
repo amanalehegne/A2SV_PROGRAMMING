4d4e15f9e9db95609ca8b6966c56bc9c7b1261a9
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-10-27 11:58:33 +0300

    Time: 710 ms (43.50%), Space: 16.6 MB (74.70%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,21 +1,12 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        prefix = count = 0
         dic = dict()
-        for i in nums:
-            prefix += i
+        prefix = count = 0
+        for i in range(len(nums)):
+            prefix += nums[i]
             if prefix == k:
                 count += 1
-            # For every prefix, there could be elements (subarray), that when removed makes it valid,
-            # and since there are -ve values, our prefix is not monotonically increasing and thus could have multiple of those points
-            # Then our count wouldn't ony increase by one, but by number of these points
             if dic.get(prefix - k):
                 count += dic.get(prefix - k)
-
-            # Putting the value in the list
-            # Since we're adding elements as we go, when we check the dictionary, we only found points up to the current position
-            if dic.get(prefix):
-                dic[prefix] += 1
-            else:
-                dic[prefix] = 1
+            dic[prefix] = 1 + dic.get(prefix, 0)
         return count
