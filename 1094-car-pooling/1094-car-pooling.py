ba4ebf252612e13b3556378efb49ac3552723ec3
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-10-27 12:22:33 +0300

    Time: 75 ms (94.82%), Space: 14.5 MB (44.15%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,14 +1,12 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        car_end = prefix = 0
-        for num, start, end in trips:
-            car_end = max(car_end, end)
-        ans = [0] * (car_end + 1)
-        for num, start, end in trips:
-            ans[start] += num
-            ans[end] += -num
-        for i, val in enumerate(ans):
-            prefix += val
+        prefix, distance = 0, max(z for x, y, z in trips)
+        ans = [0] * (distance + 1)
+        for people, start, end in trips:
+            ans[start] += people
+            ans[end] -= people
+        for i in ans:
+            prefix += i
             if prefix > capacity:
                 return False
         return True
