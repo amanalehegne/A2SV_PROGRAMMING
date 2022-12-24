483e851756462ac9328088b8cddc68c15ffc400d
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-12-24 12:21:27 +0300

    Time: 1128 ms (13.44%), Space: 14.9 MB (26.50%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.py
+++ 1089-duplicate-zeros/1089-duplicate-zeros.py
@@ -1,17 +1,15 @@
 class Solution:
     def duplicateZeros(self, arr: List[int]) -> None:
         index = 0
-        for num in arr[:]:
-            if num == 0:
+        length = len(arr)
+        while index < length:
+            if arr[index] == 0:
+                for i in range(length - 1, index, -1):
+                    arr[i] = arr[i - 1]
                 arr[index] = 0
-                index += 1
-                if index == len(arr):
-                    break
-                arr[index] = 0
+                index += 2
             else:
-                arr[index] = num
-            index += 1
-            if index == len(arr):
-                break
-        
+                index += 1
+
+
         
