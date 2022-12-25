70fbb324bd73e0119ff31436eaee03cacbcaa8fd
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-12-25 18:28:02 +0300

    Time: 25 ms (98.92%), Space: 13.8 MB (68.54%) - LeetHub

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.py
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.py
@@ -1,14 +1,26 @@
 class Solution:
     def freqAlphabets(self, s: str) -> str:
-        stack = []
-        for nums in s:
-            if nums == "#":
-                s, f = stack.pop(), stack.pop()
-                stack.append(f + s)
+        length = len(s)
+        countHash = 0
+        for char in s:
+            if char == "#":
+                countHash += 1
+
+        answer = [0] * (length - countHash*2)
+        answerIndex = 1
+        index = 0
+        while index < length:
+            if s[(length - 1 - index)] == "#":
+                index += 1
+                second = s[(length - 1 - index)]
+                index += 1
+                first = s[(length - 1 - index)]
+                answer[-answerIndex] = chr(96 + int(first + second))
             else:
-                stack.append(nums)
-        answer = []
-        for i in stack:
-            answer.append(chr(96 + int(i)))
+                answer[-answerIndex] = chr(96 + int(s[(length - 1 - index)]))
+            index += 1
+            answerIndex += 1
+
         return "".join(answer)
+        
         
