312284535c9d20346f75d149bbd2e744d6af2a8d
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-12-27 15:35:18 +0300

    Update strickSuperset.py

--- Python-Track-Hackerrank/strickSuperset.py
+++ Python-Track-Hackerrank/strickSuperset.py
@@ -1,13 +1,13 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-superset = set(list(map(int, input().split(" "))))
-setsize = int(input())
-check = True
-for i in range(setsize):
-    subset = set(list(map(int, input().split(" "))))
-    if (not superset.difference(subset)) or (subset.difference(superset)):
-        print(False)
-        check = False
-        break
+def checkSuperset(superset, subsetSize):
+    for i in range(subsetSize):
+        subset = set(list(map(int, input().split(" "))))
+        if (not superset.difference(subset)) or (subset.difference(superset)):
+            return False
 
-if check:
-    print(True)
+    return True
+
+    
+superset = set(list(map(int, input().split(" "))))
+subsetSize = int(input())
+print(checkSuperset(superset, subsetSize))
