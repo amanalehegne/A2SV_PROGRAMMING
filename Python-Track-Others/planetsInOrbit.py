94bc5a374725fbe2c593817e75c1a218d2774b4c
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2023-01-04 19:41:55 +0300

    Create planetsInOrbit.py

---
+++ Python-Track-Others/planetsInOrbit.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+def distraction(n, planets):
+    orbit = Counter(planets)
+    count = 0
+    for planet in orbit:
+        if orbit.get(planet) > 1:            
+            count += min(n, orbit.get(planet))
+        else:
+            count += 1
+    return count
+
+
+testCases = int(input())
+for i in range(testCases):
+    size, n = list(map(int, input().split(" ")))
+    planets = list(map(int, input().split(" ")))
+    print(distraction(n, planets))
