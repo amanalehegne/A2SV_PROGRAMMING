cd6a7da8e030a4baca21b13da3e1bf5b75645554
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2023-01-12 10:12:14 +0300

    Time: 185 ms (97.03%), Space: 17.5 MB (64.47%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.py
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -1,47 +1,38 @@
 class Solution:
-    def findDiagonalOrder(self, matrix: List[List[int]]) -> List[int]:
-        answer = []
-        rows = len(matrix)
-        cols = len(matrix[0])
-        answerLength = 0
-        up = True
-        rowIndex = colIndex = 0
-
-
-        while answerLength < (rows * cols):
-            if up:
-                # As we move upward our rows decreases and our column increases
-                # For decreasing we have to check if it reaches zero
-                # For increasing we check if it reaches the limit
-                while rowIndex >= 0 and colIndex < cols:
-                    answer.append(matrix[rowIndex][colIndex])
-                    answerLength += 1
-                    rowIndex -= 1
-                    colIndex += 1
-
-                # There is a possiblity we reach the corners
-                # When this happens we're off in our column by one and row by two
-                if colIndex == cols:
-                    colIndex -= 1
-                    rowIndex += 2
-                else:
-                    rowIndex += 1
-            else:
-                # As we move down, column indices are decreasing and row indices are increasing
-                # Thus we have to check if column index reach zero and row index reach the limit
-                while colIndex >= 0 and rowIndex < rows:
-                    answer.append(matrix[rowIndex][colIndex])
-                    answerLength += 1
-                    colIndex -= 1
-                    rowIndex += 1
-
-                # There is a possiblity we hit the corners
-                if rowIndex == rows:
-                    rowIndex -= 1
-                    colIndex += 2
-                else:
-                    colIndex += 1
-            up = not up
-
-        return answer
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        y = len(mat)
+        x = len(mat[0])
+        state = True
+        xIndex = yIndex = 0
+        res = []
+        resSize = 0
         
+        while resSize < x*y:
+            if state:
+                while yIndex >= 0 and xIndex < x:
+                    res.append(mat[yIndex][xIndex])
+                    resSize += 1
+                    
+                    xIndex += 1
+                    yIndex -= 1
+                
+                if xIndex == x:
+                    yIndex += 1
+                    xIndex -= 1
+                yIndex += 1
+            else:
+                while yIndex < y and xIndex >= 0:
+                    res.append(mat[yIndex][xIndex])
+                    resSize += 1
+                    
+                    xIndex -= 1
+                    yIndex += 1
+                
+                if yIndex == y:
+                    yIndex -= 1
+                    xIndex += 1
+                xIndex += 1
+            state = not state
+        
+        return res
+                
