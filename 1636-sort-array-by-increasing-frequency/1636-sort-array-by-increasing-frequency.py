1823e70aa61433418ddd65ca00529beec57e3738
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2023-01-17 19:00:37 +0300

    Time: 46 ms (97.76%), Space: 13.8 MB (63.09%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
@@ -1,24 +1,11 @@
 class Solution:
     def frequencySort(self, nums: List[int]) -> List[int]:
-        def compare(x, y):
-            # Return 1 - Don't Swap
-            # Return -1 - Swap
-            if x[1] > y[1]:
-                return 1
-            elif x[1] < y[1]:
-                return -1
-            else:
-                if x[0] > y[0]:
-                    return -1
-                else:
-                    return 1
-                
         dic = Counter(nums)
         temp = []
         for key in dic:
             temp.append([key, dic.get(key)])
         
-        temp.sort(key=cmp_to_key(compare))
+        temp.sort(key= lambda x:(x[1], -x[0]))
 
         ans = []
         for x, y in temp:
