8d4092032fc78807389986a6222b72eb6430595a
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2023-01-17 19:31:20 +0300

    Time: 803 ms (8.83%), Space: 15.7 MB (8.95%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,15 +1,24 @@
 class Solution:
     def sortPeople(self, name: List[str], height: List[int]) -> List[str]:
+        maxVal = max(height)
+        temp = [0] * maxVal
         length = len(height)
-        for i in range(1, length):
-            val = height[i]
-            val2 = name[i]
-            j = i - 1
-            while j >= 0 and val > height[j]:
-                height[j + 1] = height[j]
-                name[j + 1] = name[j]
-                j -= 1
-            height[j + 1] = val
-            name[j + 1] = val2
-            
-        return name
+
+        count = 0
+        dic = dict()
+
+        for i in range(length):
+            if temp[height[i] - 1] == 0:
+                count += 1
+            temp[height[i] - 1] += 1
+            dic[height[i] - 1] = name[i]
+
+        ans = [0] * count
+        index = count - 1
+        for i in range(maxVal):
+            size = temp[i]
+            for j in range(size):
+                ans[index] = dic.get(i)
+                index -= 1
+
+        return ans
