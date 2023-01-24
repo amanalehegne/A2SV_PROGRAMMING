971efb1f39ad0f8886d9bb2f8d4ed21a1b88d470
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2023-01-24 12:28:48 +0300

    Time: 40 ms (61.21%), Space: 13.9 MB (67.86%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -1,14 +1,11 @@
 class Solution:
-    def rotate(self, matrix: List[List[int]]) -> None:
-        cols = rows = len(matrix)
-        for row in range(rows):
-            for col in range(cols - row):
-                index = cols - 1 - col
-                matrix[index][row], matrix[row][index] = matrix[row][index], matrix[index][row]
-        for row in range(rows):
-            for col in range(cols // 2 + cols % 2):
-                index = cols - 1 - col
-                matrix[row][col], matrix[row][index] = matrix[row][index], matrix[row][col]
-        return matrix
-            
+    def rotate(self, arr: List[List[int]]) -> None:
+        rows = len(arr)
+        for idx in range(rows):
+            for i in range(idx, rows):
+                arr[idx][i], arr[i][idx] = arr[i][idx], arr[idx][i]
+        
+        for lst in arr:
+            lst.reverse()
+        
         
