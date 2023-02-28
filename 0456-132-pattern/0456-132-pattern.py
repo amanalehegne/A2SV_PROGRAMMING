5f773e3c944e4fcdd32d7dbd92b855577b7cbe09
Amanuel Alehegne Wubete <88857179+amanalehegne@users.noreply.github.com>
2023-02-28 15:53:34 +0300

    Time: 356 ms (78.29%), Space: 32 MB (94.02%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.py
+++ 0456-132-pattern/0456-132-pattern.py
@@ -2,13 +2,21 @@ class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
         stack = []
         length = len(nums)
-        kValue = -float("inf")
+        jValue = -float("inf")
         for i in range(length):
             idx = length - 1 - i
-            if nums[idx] < kValue:
+            if nums[idx] < jValue:
+                # the top of our stack is the K value
+                # Since we are going backward, the nums[idx] if the I value
+                # jVal is the J value
+                # Becuse it's a monotonically decreasing stack, we can be sure that jVal < top of stack (stack[-1])
+                # if I value (nums[idx]) < J value (jVal), we get the patter nums[idx] (I) < jVal (J) < stack[-1] (K) or 132 => IKJ
                 return True
             while stack and stack[-1] < nums[idx]:
-                kValue = stack.pop()
+                # Since nums[j] > nums[i], we have to maximize the jValue
+                # Because we have a monotonic stack (decreasing), the first element is the smallest and the last is the greatest
+                # We want to maiximize the jVal as high as possible
+                jValue = stack.pop()
                 
             stack.append(nums[idx])
         return False
