00e5cda4249245dcfe70bebb684af63ab18e0c5b
Amanuel Alehegne Wubete <88857179+amanalehegne@users.noreply.github.com>
2023-02-28 21:07:33 +0300

    Time: 25 ms (94.60%), Space: 14 MB (12.09%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,22 @@
+class Solution:
+    def calc(self, x, n):
+        if not x: return 0
+        if not n: return 1
+        
+        res = self.calc(x, n // 2)
+        res = res * res
+        
+        if n % 2:
+            return res * x
+        else:
+            return res
+    
+    def myPow(self, x: float, n: int) -> float:
+        temp = n
+        ans = self.calc(x, abs(n))
+        if temp < 0:
+            return 1 / ans
+        return ans
+        
+        
+        
