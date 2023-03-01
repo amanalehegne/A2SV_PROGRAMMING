9027043c7ef12880b7c06045a0e321aa3618df03
Amanuel Alehegne Wubete <88857179+amanalehegne@users.noreply.github.com>
2023-03-01 18:23:09 +0300

    Time: 138 ms (67.04%), Space: 14.6 MB (99.18%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,24 +1,29 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        base = Counter(t)
-        baseLength = len(base)
         length = len(s)
+        base = Counter(t)
+        baseSize = len(base)
         dic = defaultdict(int)
-        left = 0
-        check = 0
+        completeChars = 0
         res = [float("inf"), 0, 0]
+        left = 0
         
         for i in range(length):
             dic[s[i]] += 1
             if s[i] in base and dic[s[i]] == base[s[i]]:
-                check += 1
-            while check == baseLength:
+                completeChars += 1
+            
+            while completeChars == baseSize:
                 if res[0] > (i - left + 1):
-                    res = [(i - left + 1), left, i+1]
+                    res = [(i - left + 1), left, i]
                 dic[s[left]] -= 1
-                if s[i] in base and dic[s[left]] < base[s[left]]:
-                        check -= 1
+                if s[left] in base and dic[s[left]] < base[s[left]]:
+                    completeChars -= 1
                 left += 1
                 
-        return s[res[1]:res[-1]]
-        
+        if res[0] == float("inf"):
+            return ""
+        return s[res[1] : res[-1] + 1]
+                
+                
+            
