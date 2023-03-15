1d01c9bd565743e3cbb0a063379e96de50340a53
Amanuel Alehegne Wubete <88857179+amanalehegne@users.noreply.github.com>
2023-03-15 18:38:12 +0300

    Time: 37 ms (48.17%), Space: 13.9 MB (42.46%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,14 +1,15 @@
 class Solution:
-    def memo(self, n, dic):
-        if n == 0 or n == 1:
-            return n
-        if dic.get(n):
-            return dic[n]
-        else:
-            dic[n] = self.memo(n - 1, dic) + self.memo(n - 2, dic)
-            return dic[n]
-        
     def fib(self, n: int) -> int:
-        dic = dict()
-        return self.memo(n, dic)
+        arr = [1, 1]
+        if n == 0 or n == 1: return n
+
+        def rec(n):
+            if n - 2 == 0:
+                return
+            cur = arr[-1] + arr[-2]
+            arr.append(cur)
+            rec(n - 1)
+
+        rec(n)
+        return arr[-1]
         
