8b450784eab08afa4f878171ef8baed681edb753
Amanuel Alehegne Wubete <88857179+amanalehegne@users.noreply.github.com>
2023-03-15 16:57:00 +0300

    Time: 9382 ms (5.02%), Space: 13.9 MB (48.02%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,24 +1,20 @@
 class Solution:
-    def PredictTheWinnerRec(self, nums, left, right):
-        if left > right: return 0
-        
-        leftOnly = self.PredictTheWinnerRec(nums, left + 2, right)
-        leftRight = self.PredictTheWinnerRec(nums, left + 1, right - 1)
-        
-        moveLeft = nums[left] + min(leftOnly, leftRight)
-        
-        rightOnly = self.PredictTheWinnerRec(nums, left, right - 2)
-        
-        moveRight = nums[right] + min(rightOnly, leftRight)
-        
-        return max(moveLeft, moveRight)
-        
     def PredictTheWinner(self, nums: List[int]) -> bool:
-        totalScore = sum(nums)
-        player1 = self.PredictTheWinnerRec(nums, 0, len(nums) - 1)
-        player2 = totalScore - player1
+        def helper(left, right, turn):
+            # If we are out of elements, nobody gets point
+            if left > right:
+                return 0
+            # If its our turn, we want to maximize our points and mimize the point our oponent could get because we are concerend in the existance of a possiblity for player one to win, and the way to check that would to increase player ones score and much as possible and decrease players twos
+            if turn:
+                # In every turn we have 2 choices, either to get the left or the right element
+                return max(nums[left] + helper(left + 1, right, not turn), nums[right] + helper(left, right - 1, not turn))
+            else:
+                return min(-nums[left] + helper(left + 1, right, not turn), -nums[right] + helper(left, right - 1, not turn))
         
-        if player2 > player1:
+        # What we are doing is that when player one get a point we add it and when player 2 gets a point we subtract it, and if the final answer is positive or zero it means we have a possiblity where player one wins, otherwise in all possiblity player two wins
+        val = helper(0, len(nums) - 1, True)
+        if val < 0:
             return False
         return True
+                
         
