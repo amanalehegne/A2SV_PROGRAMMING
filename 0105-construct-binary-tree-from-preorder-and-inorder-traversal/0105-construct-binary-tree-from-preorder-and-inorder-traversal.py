1d4d13cde66df7a481224416fd463d6ca2db38b6
Amanuel Alehegne Wubete <88857179+amanalehegne@users.noreply.github.com>
2023-03-16 21:38:34 +0300

    Time: 181 ms (58.21%), Space: 88.8 MB (11.81%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -12,9 +12,13 @@ class Solution:
                 return None
             
             parent = preorder[0]
+            # The first element will be our parent node, and the very first element in preorder is the root node
             parentNode = TreeNode(parent)
+            # LNR (left, node, right) if how we traverse inorder
+            # This means if we find the the position of the current node, all element left to it will be placed in the left side and all the elements to the right will be in nodes right side
             partition = inorder.index(parent)
             
+            # We partition the list into left and right recursively
             left = helper(preorder[1:partition + 1], inorder[:partition])
             right = helper(preorder[partition + 1:], inorder[partition + 1:])
             
