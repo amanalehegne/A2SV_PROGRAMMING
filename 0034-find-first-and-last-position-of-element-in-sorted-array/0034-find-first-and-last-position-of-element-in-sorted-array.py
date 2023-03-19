909fdaedcf2d40bf694989eed63837e4c4bec220
Amanuel Alehegne Wubete <88857179+amanalehegne@users.noreply.github.com>
2023-03-19 11:03:11 +0300

    Time: 86 ms (73.81%), Space: 15.4 MB (87.24%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,36 +1,25 @@
 class Solution:
-    def searchRange(self, arr: List[int], k: int) -> List[int]:
-        left = 0
-        right = len(arr) - 1
-        res = []
-        if not arr:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        def leftBisect(nums, target):
+            left = 0
+            right = len(nums) - 1
+            
+            while left <= right:
+                midPoint = left + (right - left) // 2
+                
+                if nums[midPoint] >= target:
+                    right = midPoint - 1
+                else:
+                    left = midPoint + 1
+                
+            return left
+        
+        left = leftBisect(nums, target)
+        right = leftBisect(nums, target + 1) - 1 
+
+        if left > right:
             return [-1, -1]
         
-        while left <= right:
-            midPoint = left + (right - left) // 2
-            if arr[midPoint] < k:
-                left = midPoint + 1
-            else:
-                right = midPoint - 1
-                
-        if left >= len(arr) or arr[left] != k:
-            left = -1
-        res.append(left)
-        
-        
-        left = 0
-        right = len(arr) - 1
-        while left <= right:
-            midPoint = left + (right - left) // 2
-            if arr[midPoint] > k:
-                right = midPoint - 1
-            else:
-                left = midPoint + 1
+        return [left, right]
+                    
             
-        
-        if right < 0 or arr[right] != k:
-            right = -1
-        res.append(right)
-    
-        return res
-        
