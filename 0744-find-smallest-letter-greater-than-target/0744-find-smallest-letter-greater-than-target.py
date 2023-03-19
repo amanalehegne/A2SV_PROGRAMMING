497cadf03e8272ad9ac4fdce4ce9eb1fe7baa055
Amanuel Alehegne Wubete <88857179+amanalehegne@users.noreply.github.com>
2023-03-19 10:45:09 +0300

    Time: 110 ms (71.98%), Space: 14.3 MB (71.66%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.py
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.py
@@ -3,17 +3,14 @@ class Solution:
         left = 0
         right = len(letters) - 1
 
-        target = chr(ord(target) + 1)
-
         while left <= right:
             midPoint = left + (right - left) // 2
-
-            if letters[midPoint] < target:
-                left = midPoint + 1
-            else:
+            if letters[midPoint] > target:
                 right = midPoint - 1
+            else:
+                left = midPoint + 1
 
         if left > len(letters) - 1:
-            return letters[0]
+            left = 0
         return letters[left]
         
