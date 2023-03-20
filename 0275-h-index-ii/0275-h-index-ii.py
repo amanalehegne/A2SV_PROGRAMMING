1acf4aca5c4501ceeab5eb55fa71870d43ebccae
Amanuel Alehegne Wubete <88857179+amanalehegne@users.noreply.github.com>
2023-03-20 20:50:12 +0300

    Time: 143 ms (68.93%), Space: 20.6 MB (72.53%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.py
+++ 0275-h-index-ii/0275-h-index-ii.py
@@ -1,7 +1,7 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        length = len(citations)
         
+        length = len(citations)
         left = 0
         right = length - 1
         
