f3946a3b3608c3945c070942186f8b2e93dd2e8a
Amanuel Alehegne Wubete <88857179+amanalehegne@users.noreply.github.com>
2023-03-22 13:24:13 +0300

    Time: 4401 ms (18.99%), Space: 13.9 MB (67.25%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -3,17 +3,17 @@ class Solution:
         res = [float("inf")]
         people = [0] * k
         
-        def backtrack(idx):
+        def backtrack(idx, current_max):
             if idx >= len(cookies):
-                res[0] = min(res[0], max(people))
+                res[0] = min(res[0], current_max)
                 return
             
             length = len(people)
             for i in range(length):
                 people[i] += cookies[idx]
                 if people[i] < res[0]:
-                    backtrack(idx + 1)
+                    backtrack(idx + 1, max(current_max, people[i]))
                 people[i] -= cookies[idx]
         
-        backtrack(0)
+        backtrack(0, 0)
         return res[0]
