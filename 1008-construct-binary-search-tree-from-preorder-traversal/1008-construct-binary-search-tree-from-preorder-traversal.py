e3049fb6f8e8930272fe427558821a3d1f88c808
Amanuel Alehegne Wubete <88857179+amanalehegne@users.noreply.github.com>
2023-03-26 09:18:57 +0300

    Time: 43 ms (27.98%), Space: 13.9 MB (83.37%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.py
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.py
@@ -4,40 +4,36 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]:
-        def binarySearch(arr, key):
+        def getIndex(lst, key):
+            arr = sorted(lst)
             left = 0
             right = len(arr) - 1
-            
             while left <= right:
                 midPoint = left + (right - left) // 2
                 
-                if arr[midPoint] == key:
-                    return midPoint
                 if arr[midPoint] > key:
                     right = midPoint - 1
                 else:
                     left = midPoint + 1
             
-            return 0
+            return right
         
-            
-        def helper(preorder, inorder):
-            if not preorder:
+        def helper(arr):
+            if not arr:
                 return None
             
-            parent = preorder[0]
-            partition = binarySearch(inorder, parent)
+            parent = arr[0]
+            split = getIndex(arr, parent)
             
-            left = helper(preorder[1:partition + 1], inorder[:partition])
-            right = helper(preorder[partition + 1:], inorder[partition + 1:])
+            left = helper(arr[1:split+1])
+            right = helper(arr[split+1:])
             
             node = TreeNode(parent, left, right)
             
             return node
-        
-        res = helper(preorder, sorted(preorder))
-        return res
-        
-        
+            
+            
+        return helper(preorder)
