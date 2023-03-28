fc0809cdac7fdcb807a4561d7fb8b16ef829d882
Amanuel Alehegne Wubete <88857179+amanalehegne@users.noreply.github.com>
2023-03-28 08:17:12 +0300

    Time: 1279 ms (21.01%), Space: 30.2 MB (24.31%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,9 +1,5 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        """
-        The time complexity would be on avergae case O(n) -> because we are only concerned with only onw half of the each list which means n + n/2 + n/4 + n/8 ... = 2n
-        In the worst case it is O(n^2) because we are using quick sort
-        """
         def partition(arr, left, right):
             start = left
             pivot = arr[start]
@@ -29,14 +25,14 @@ class Solution:
             idx = partition(arr, left, right)
 
             length = len(arr)
-            largest = length - k
+            res = length - k
 
-            if largest > idx:
+            if res > idx:
                 return quickSelect(arr, idx + 1, right)
-            elif largest < idx:
+            elif res < idx:
                 return quickSelect(arr, left, idx - 1)
             else:
-                return arr[largest]
+                return arr[res]
         
         return quickSelect(nums)
         
