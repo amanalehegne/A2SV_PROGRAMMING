cc7ef42f4209514b04da83f6707177aa514c16f0
Amanuel Alehegne Wubete <88857179+amanalehegne@users.noreply.github.com>
2023-04-07 19:29:02 +0300

    Time: 693 ms (14.80%), Space: 20.5 MB (21.05%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,40 +1,41 @@
 class Solution:
-    def islandPerimeter(self, grid: List[List[int]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
+    def findFirstOne(self, grid):
+            rows = len(grid)
+            cols = len(grid[0])
         
-        def findFirstOne(rows, cols):
             for row in range(rows):
                 for col in range(cols):
                     if grid[row][col] == 1:
                         return [row, col]
+                    
+                    
+    def check(self, row, col, grid):
+        cols = 0 <= col < len(grid[0])
+        rows = 0 <= row < len(grid)
+        return cols and rows
         
-        row, col = findFirstOne(rows, cols)
+    def dfs(self, row, col, direction, grid, seen):
+
+        seen.add((row, col))
+        count = 0
+
+        for row_, col_ in direction:
+            newRow = row + row_
+            newCol = col + col_
+            validate = self.check(newRow, newCol, grid)
+
+            if not validate or grid[newRow][newCol] == 0:
+                count += 1
+            elif validate and (newRow, newCol) not in seen and grid[newRow][newCol] == 1:
+                count += self.dfs(newRow, newCol, direction, grid, seen)
+
+        return count
+                    
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        
+        row, col = self.findFirstOne(grid)
         
         direction = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-        
-        def check(row, col):
-            cols = 0 <= col < len(grid[0])
-            rows = 0 <= row < len(grid)
-            return cols and rows
-    
         seen = set()
         
-        def dfs(row, col):
-            
-            seen.add((row, col))
-            count = 0
-
-            for row_, col_ in direction:
-                newRow = row + row_
-                newCol = col + col_
-                validate = check(newRow, newCol)
-                
-                if not validate or grid[newRow][newCol] == 0:
-                    count += 1
-                elif validate and (newRow, newCol) not in seen and grid[newRow][newCol] == 1:
-                    count += dfs(newRow, newCol)
-            
-            return count
-        
-        return dfs(row, col)
+        return self.dfs(row, col, direction, grid, seen)
