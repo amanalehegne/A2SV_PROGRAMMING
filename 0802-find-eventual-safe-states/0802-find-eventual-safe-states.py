a25fededbc6e6538b648bc50d0753a8f9a03c846
Amanuel Alehegne Wubete <88857179+amanalehegne@users.noreply.github.com>
2023-05-13 23:03:45 +0300

    Time: 645 ms (89.43%), Space: 23.5 MB (42.16%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -1,18 +1,25 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        def dfs(node, color, graph):
-            color[node] = 1 # Current Path
-            for child in graph[node]:
-                if color[child] == 1 or (color[child] == 0 and not dfs(child, color, graph)):
-                    return False
+        def DFS(graph, node, color, res):
+            if color[node] == 0:
+                color[node] = 1
+                children = graph[node]
+                for child in children:
+                    if color[child] == 0:
+                        val = DFS(graph, child, color, res)
+                        if not val:
+                            return val
+                    elif color[child] == 1:
+                        return False
 
-            color[node] = 2 # Visited
-            return True
-
-        color = [0] * len(graph)
+                color[node] = 2
+                res.append(node)
+                return True
+        
+        size = len(graph)
+        color = [0] * size
         res = []
-        for i in range(len(graph)):
-            if dfs(i, color, graph):
-                res.append(i)
-
-        return res
+        for node in range(size):
+            DFS(graph, node, color, res)
+        
+        return sorted(res)
