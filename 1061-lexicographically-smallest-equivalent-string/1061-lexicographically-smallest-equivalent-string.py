00762b236d0cf5ed68f3665f3aca9324e5542579
Amanuel Alehegne Wubete <88857179+amanalehegne@users.noreply.github.com>
2023-05-21 10:03:10 +0300

    Time: 65 ms (17.01%), Space: 16.5 MB (17.56%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
@@ -1,35 +1,32 @@
 class Solution:
     def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str: 
-        # Get the alphabets from a to z
-        rep = {chr(i) : chr(i) for i in range(97, 123)}
+        parent = dict()
+        
+        def find(node):
+            if node not in parent:
+                parent[node] = node
+            elif node != parent[node]:
+                parent[node] = find(parent[node])
+            return parent[node]
+        
+        def union(node1, node2):
+            parent1 = find(node1)
+            parent2 = find(node2)
 
-        
-        def find(rep, node):
-            if node == rep[node]:
-                return node
-            rep[node] = find(rep, rep[node])
-            return rep[node]
-        
-        def union(rep, node1, node2):
-            node1 = find(rep, node1)
-            node2 = find(rep, node2)
-            
-            # We choose the lexographically smallest parent to represent the union of the two groups
-            val = min(node1, node2)
-            if node1 != node2:
-                rep[node1] = val
-                rep[node2] = val
+            if parent1 != parent2:
+                if parent1 < parent2:
+                    parent[parent2] = parent1
+                else:
+                    parent[parent1] = parent2
         
         sizeStr = len(s1)
         for i in range(sizeStr):
-            node1 = s1[i]
-            node2 = s2[i]
-            union(rep, node1, node2)
-        
-        ans = []
+            union(s1[i], s2[i])
+            
+        res = []
         for char in baseStr:
-            # Get the parent (which is the smallest lexographically) of the current character
-            ans.append(find(rep, char))
+            res.append(find(char))
         
-        return "".join(ans)
+        return "".join(res)
+            
         
