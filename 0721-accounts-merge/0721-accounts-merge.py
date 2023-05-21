68fc23b1e9cc60355740c79a42e8693347c52688
Amanuel Alehegne Wubete <88857179+amanalehegne@users.noreply.github.com>
2023-05-21 11:28:45 +0300

    Time: 257 ms (25.32%), Space: 21.4 MB (34.18%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.py
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -1,56 +1,55 @@
-from typing import List
-
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
-        rep = dict()
+        parent = dict()
         size = dict()
+        
+        def find(node):
+            if node not in parent:
+                parent[node] = node
+                size[node] = 1
+            elif node != parent[node]:
+                parent[node] = find(parent[node])
+            return parent[node]
+        
+        def union(node1, node2):
+            parent1 = find(node1)
+            parent2 = find(node2)
+            
+            if parent1 != parent2:
+                if size[parent1] < size[parent2]:
+                    parent1, parent2 = parent2, parent1
 
+                parent[parent2] = parent1
+                size[parent1] += size[parent2]
+        
+        def groupItems():
+            res = defaultdict(list)
+            for node in parent:
+                res[find(node)].append(node)
+            
+            return res
+            
+        users = dict()
         for account in accounts:
-            length = len(account)
-            for idx in range(1, length):
-                pair = (account[idx], account[0])
-                rep[pair] = pair
-                size[pair] = 1
-
-        def find(rep, pair):
-            if pair == rep[pair]:
-                return pair
-            rep[pair] = find(rep, rep[pair])
-            return rep[pair]
-
-        def union(rep, size, pair1, pair2):
-            pair1 = find(rep, pair1)
-            pair2 = find(rep, pair2)
-
-            if pair1 == pair2:
-                return
-            size1, size2 = size[pair1], size[pair2]
-            if size1 > size2:
-                pair1, pair2 = pair2, pair1
-
-            rep[pair1] = pair2
-            size[pair2] += size[pair1]
-
-        for account in accounts:
-            length = len(account)
-            base = (account[1], account[0])
-            for idx in range(2, length):
-                union(rep, size, base, (account[idx], account[0]))
-
-        merged = dict()
-        for account in accounts:
-            length = len(account)
-            for idx in range(1, length):
-                email = account[idx]
-                root = find(rep, (email, account[0]))
-                if root not in merged:
-                    merged[root] = set()
-                merged[root].add(email)
-
+            accountSize = len(account)
+            for i in range(1, accountSize):
+                find(account[i])
+                users[account[i]] = account[0]
+                if i + 1 < accountSize:
+                    union(account[i], account[i + 1])
+        
+        groups = groupItems()
         res = []
-        for root in merged:
-            emails = merged.get(root)
-            res.append([root[1]] + sorted(emails))
-
-
+        for key in groups:
+            res.append([users[key]] + sorted(groups[key]))
+        
         return res
+        
+
+        
+        
+        
+        
+                
+        
+            
