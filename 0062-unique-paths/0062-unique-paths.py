6ed58298086f2d2e52807f91991a39a67ca2984e
Amanuel Alehegne Wubete <88857179+amanalehegne@users.noreply.github.com>
2023-05-31 18:46:14 +0300

    Time: 60 ms (6.41%), Space: 16.3 MB (43.78%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,8 +1,8 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
 
-        def isValidPath(row, col):
-            return 0 <= row < m and 0 <= col < n
+        def isValidPosition(grid, node):
+            return 0 <= node[0] < len(grid) and 0 <= node[1] < len(grid[0])
 
         
         grid = [[0 for _ in range(n)] for _ in range(m)]
@@ -11,10 +11,10 @@ class Solution:
         for row in range(m):
             for col in range(n):
                 rowVal = colVal = 0
-                if isValidPath(row - 1, col):
-                    rowVal = grid[row - 1][col]
-                if isValidPath(row, col - 1):
+                if isValidPosition(grid, (row, col - 1)):
                     colVal = grid[row][col - 1]
+                if isValidPosition(grid, (row - 1, col)):
+                    rowVal = grid[row - 1][col]
     
                 grid[row][col] += rowVal + colVal
                 
