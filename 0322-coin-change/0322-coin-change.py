248c074a3f80c6cc212810f01084c8e93ecd5eae
Amanuel Alehegne Wubete <88857179+amanalehegne@users.noreply.github.com>
2023-06-08 20:35:10 +0300

    Time: 1501 ms (38.41%), Space: 28.3 MB (21.23%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -3,16 +3,19 @@ class Solution:
         def rec(x, coins, dic):
             if x in dic:
                 return dic[x]
-            
+            if x < 0:
+                return float('inf')
+            if x == 0:
+                return 0
             res = float('inf')
             for coin in coins:
-                if x - coin >= 0:
-                    res = min(res, rec(x - coin, coins, dic))
+                res = min(res, rec(x - coin, coins, dic))
+
             dic[x] = res + 1
             return dic[x]
             
         
-        res = rec(amount, coins, {0: 0})
+        res = rec(amount, coins, dict())
         if res == float('inf'):
             res = -1
         return res
