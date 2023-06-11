fe175c93cc40b868d02298a23525516c6d60d2ba
Amanuel Alehegne Wubete <88857179+amanalehegne@users.noreply.github.com>
2023-06-11 22:23:38 +0300

    Time: 58 ms (5.28%), Space: 16.4 MB (18.22%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -1,14 +1,11 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        def rec(n, dic):
-            if n == 0:
-                return 0
-            if n < 3:
-                return 1
-            
-            if n in dic:
-                return dic[n]
-            dic[n] = rec(n - 1, dic) + rec(n - 2, dic) + rec(n - 3, dic)
-            return dic[n]
-        
-        return rec(n, dict())
+        if n == 0:
+            return 0
+        if n == 1 or n == 2:
+            return 1
+        one, two, three = 0, 1, 1
+        for i in range(n - 2):
+            current = one + two + three
+            one, two, three = two, three, current
+        return three
