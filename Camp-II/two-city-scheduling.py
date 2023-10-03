180f1c19daf080e9c304f9221d2906e368cc544b
Amanuel Alehegne Wubete <88857179+amanalehegne@users.noreply.github.com>
2023-10-03 09:52:13 +0300

    Added solution for Two City Scheduling

---
+++ Camp-II/two-city-scheduling.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        costs.sort(key=lambda x:x[0] - x[1])
+        size = len(costs)
+        res = 0
+        for i in range(size):
+            if i < size // 2:
+                res += costs[i][0]
+            else:
+                res += costs[i][1]
+        
+        return res
+    
