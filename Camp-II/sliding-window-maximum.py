14b9397cfbc5567b450d0fe1fba04c01e8942287
Amanuel Alehegne Wubete <88857179+amanalehegne@users.noreply.github.com>
2023-10-04 20:41:01 +0300

    Added solution for Sliding Window Maximum

---
+++ Camp-II/sliding-window-maximum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        queue=deque()
+        result=[]
+        for i in range(k):
+            while queue and  nums[i]>=nums[queue[-1]]:
+                queue.pop()
+            queue.append(i)
+        result.append(nums[queue[0]])
+
+        for i in range(k,len(nums)):
+            if queue and queue[0] == i-k:
+                queue.popleft()
+            
+            while queue and nums[i] >= nums[queue[-1]]:
+                queue.pop()
+            
+            queue.append(i)
+            result.append(nums[queue[0]])
+        
+        return result
