bf5e05609e92543bd9115ad00261fbf64d4a4859
Amanuel Alehegne Wubete <88857179+amanalehegne@users.noreply.github.com>
2023-10-05 14:19:57 +0300

    Added solution for Course Schedule IV

---
+++ Camp-II/course-schedule-iv.py
@@ -0,0 +1,18 @@
+class Solution:
+    def checkIfPrerequisite(self, n: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+      graph = [[False] * n for _ in range(n)]
+      for i, j in prerequisites:
+          graph[i][j] = True
+
+
+      for k in range(n):
+          for i in range(n):
+              for j in range(n):
+                  graph[i][j] = graph[i][j] or (graph[i][k] and graph[k][j])
+
+      res = []
+      for x, y in queries:
+          res.append(graph[x][y])
+      
+      return res
+        
